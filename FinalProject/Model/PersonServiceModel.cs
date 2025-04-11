da5cc927dbcc7d24b2ceaa10b6e331c52f2d7d37
Erfnlotfi <efi137986@gmail.com>
2025-04-11 14:51:44 +0330

    i arrive an i fix the past parts problems in construction

---
+++ FinalProject/Model/PersonServiceModel.cs
@@ -0,0 +1,35 @@
+﻿using Model.DomainModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+   public class PersonServiceModel
+    {
+        public List<Person> SelectAll()
+        {
+            using (var Context = new FinalProjectDbContext())
+            {
+                try
+                {
+                    var People = Context.Person.ToList();
+                    return People;
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+                finally
+                {
+                    if (Context != null)
+                        Context.Dispose();
+                }
+            }
+
+        }
+    }
+}
---
+++ FinalProject/Service/PersonService.cs
@@ -0,0 +1,37 @@
+﻿using Model;
+using Model.DomainModels;
+using Service.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+   public class PersonService
+    {
+     private readonly PersonServiceModel _personServiceModel;
+        public PersonService()
+        {
+            _personServiceModel = new PersonServiceModel();
+        }
+        public List<Person> GetAll()
+        {
+            var persons = _personServiceModel.SelectAll();
+            var getPersonDto = new List<GetPersonDto>();
+            foreach (var item in persons)
+            {
+                var gpd = new GetPersonDto()
+                {
+                    Id = item.Id,
+                    FirstName = item.FirstName,
+                    LastName = item.LastName,
+                    Nationalid = item.NationalId,
+                };
+                getPersonDto.Add(gpd);
+            }
+            return persons;
+        }
+    }
+}
